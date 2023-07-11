eb947c4c997d60eaa41e89acc766e3379699ceb3
Ahsan Habib <ahnazir.ahn@gmail.com>
2023-07-11 22:25:07 +0600

    Assignment

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+let name = "John";
+let age = 25;
+let isStudent = true;
+let hobbies = ["reading", "painting", "hiking"];
+let address = {
+  street: "123 Main Street",
+  city: "Cityville",
+  country: "Countryland"
+};
+
+console.log("My name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("I am a student: " + isStudent + ".");
+console.log("My hobbies are " + hobbies.join(", ") + ".");
+console.log("I live at " + address.street + ", " + address.city + ", " + address.country + ".");
---
+++ variables.js
@@ -0,0 +1,8 @@
+let age = 25;
+let name = "John";
+let isStudent = true;
+let birthYear;
+
+console.log("My name is " + name + " and I am " + age + " years old.");
+console.log("I am a student: " + isStudent + ".");
+console.log("I was born in " + birthYear + ".");
